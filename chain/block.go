2f5fbe7e5762a7e34bccc2c13a8febd6d88274f3
Ivan <vanzag1738@ya.ru>
2025-06-25 19:30:50 +0000

    Blockchain as a database implementation

--- chain/block.go
+++ chain/block.go
@@ -4,6 +4,7 @@ import (
 	"time"
 )
 
+// Define the block structure
 type Block struct {
 	Number     uint64
 	Time       time.Time
@@ -12,26 +13,45 @@ type Block struct {
 	Data       string
 }
 
-func GenesisBlock() Block {
-	block := Block{
+// Create and return first block in the chain without parent
+func GenesisBlock() *Block {
+	block := &Block{
 		Number:     0,
 		Time:       time.Now(),
 		ParentHash: Hash{},
 		Data:       "Hey there, I am the Genesis Block for GopherChain",
 	}
-	block.Hash = NewHash(block)
+	block.Hash = NewHash(*block)
 
 	return block
 }
 
-func NewBlock(parentBlock Block, data string) Block {
-	block := Block{
+// Create and return all other blocks in the chain
+func NewBlock(parentBlock Block, data string) *Block {
+	block := &Block{
 		Number:     parentBlock.Number + 1,
 		Time:       time.Now(),
 		ParentHash: parentBlock.Hash,
 		Data:       data,
 	}
-	block.Hash = NewHash(block)
+	block.Hash = NewHash(*block)
 
 	return block
+}
+
+// Blockchain is a database that keeps all its blocks
+type Blockchain struct {
+	blocks []*Block
+}
+
+// Save provided data as a block in the blockchain
+func (bc *Blockchain) AddBlock(data string) {
+	lastBlock := bc.blocks[len(bc.blocks)-1]
+	newBlock := NewBlock(*lastBlock, data)
+	bc.blocks = append(bc.blocks, newBlock)
+}
+
+// Create a new blockchain, starting with genesis block
+func NewBlockchain() *Blockchain {
+	return &Blockchain{[]*Block{GenesisBlock()}}
 }
--- chain/hash.go
+++ chain/hash.go
@@ -8,6 +8,7 @@ import (
 
 type Hash [32]byte
 
+// Calculate hash
 func NewHash(val any) Hash {
 	jval, err := json.Marshal(val)
 	if err != nil {
@@ -17,6 +18,7 @@ func NewHash(val any) Hash {
 	return sha256.Sum256(jval)
 }
 
+// Represent hash as a string
 func (h Hash) String() string {
 	return hex.EncodeToString(h[:])
 }
